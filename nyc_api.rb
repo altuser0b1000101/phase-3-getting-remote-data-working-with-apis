dd519838bc49618f870e3fddb485b85d3a872581
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-07-11 23:39:31 -0400

    commit tree 13643d5d87f98dd6cb53101d1ddf69fa28fbd088

--- nyc_api.rb
+++ nyc_api.rb
@@ -1,6 +1,6 @@
 require 'net/http'
- require 'open-uri'
- require 'json'
+require 'open-uri'
+require 'json'
  
  class GetPrograms
 
@@ -13,13 +13,12 @@ require 'net/http'
   end
 
   def program_school
-    programs = JSON.parse(self.get_programs)
-    programs.collect do |program|
-      program["agency"]
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]  
+      end
     end
-  end
 
-end
-
- programs = GetPrograms.new.get_programs
- puts programs
+    programs = Getprograms.new
+    puts programs.program_school.uniq
